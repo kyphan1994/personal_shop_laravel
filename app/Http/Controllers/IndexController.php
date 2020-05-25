fa591730f022dc991124e88f5b9e87117ea5905a
Ky Phan <kyphan1994@gmail.com>
2020-05-25 17:14:51 +0700

    doneallbanner

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Banners;
+use App\Category;
+use App\Products;
 
 class IndexController extends Controller
 {
     public function index() {
-        return view('pershop.index');
+        $banners = Banners::where('status', '1')->orderby('sort_order', 'asc')->get();
+        $categories = Category::with('categories')->where(['parent_id'=>0])->get();
+        $products = Products::get();
+        return view('pershop.index')->with(compact('banners', 'categories', 'products'));
     }
 }
